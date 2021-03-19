441934d7bb96bcc30ee17dc3e3e917ee7566cf60
Gabriel Pereira de Menezes <gabrielpereiram10@gmail.com>
2021-03-19 19:08:55 -0300

    add constraint list

---
+++ allocation/constraints/__init__.py
@@ -0,0 +1,10 @@
+from .just_a_schedule import JustAScheduleConstraint
+from .semester import SemesterConstraint
+from .professor import ProfessorConstraint
+
+
+constraints = [
+    JustAScheduleConstraint,
+    SemesterConstraint,
+    ProfessorConstraint
+]
