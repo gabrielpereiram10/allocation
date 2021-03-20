7df17d8945860af25148187b432d1faf51fc4f9a
Gabriel Pereira de Menezes <gabrielpereiram10@gmail.com>
2021-03-20 20:43:41 -0300

    define adapter for pysat

---
+++ allocation/sats/pysat_adapter.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from pysat.solvers import Glucose4
+
+from allocation.protocols.sat import SAT
+from allocation.protocols.types import Interpretation, ClausesOfIntegers
+from allocation.protocols.interp_converter import InterpConverter
+
+
+class PySATAdapter(SAT):
+    def __init__(self, clauses: ClausesOfIntegers, converter: InterpConverter):
+        self._clauses = clauses
+        self._converter = converter
+        self._solver = Glucose4()
+
+    def is_satisfiable(self) -> Union[Interpretation, bool]:
+        [self._solver.add_clause(list(clause)) for clause in self._clauses]
+        if self._solver.solve():
+            return self._converter.to_interp(set(self._solver.get_model()))
+        return False
