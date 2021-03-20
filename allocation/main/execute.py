e60ed58f5db094a20c743174e8c8e5367acc196d
Gabriel Pereira de Menezes <gabrielpereiram10@gmail.com>
2021-03-20 20:48:13 -0300

    test main

---
+++ allocation/main/execute.py
@@ -0,0 +1,42 @@
+import time
+from typing import List
+
+from allocation.protocols.types import Disciplines, Schedules
+from allocation.protocols.sat import SAT
+
+
+class Allocator:
+    def __init__(self, disciplines: Disciplines, n_schedules: int):
+        self._disciplines = disciplines
+        self._n_schedules = n_schedules
+
+    def execute(self, sats: List[SAT]):
+        for sat in sats:
+            start = time.time()
+            result = sat.is_satisfiable()
+            end = time.time()
+            print(f'Time: {end - start}')
+            if result:
+                self.show(self.interpretation_adapt(result))
+            else:
+                print(False)
+
+    def interpretation_adapt(self, interp):
+        disciplines_copy = self._disciplines.copy()
+        schedules = [[] for i in range(self._n_schedules)]
+        interp = set(filter(lambda t: t[1] is True, interp))
+        for i in interp:
+            name = i[0].name.split('_')
+            schedules[int(name[1]) - 1].append(disciplines_copy[int(name[0]) - 1])
+        return schedules
+
+    @staticmethod
+    def show(schedules):
+        for i, schedule in enumerate(schedules):
+            print(f'{i + 1}: ', end='')
+            for j, discipline in enumerate(schedule):
+                if j == len(schedule) - 1:
+                    print(f'{discipline}', end='.')
+                else:
+                    print(f'{discipline}', end=', ')
+            print()
---
+++ allocation/main/main.py
@@ -0,0 +1,55 @@
+import timeit
+from allocation.main.get_disciplines import get_disciplines
+from allocation.entities.formula import Atom
+from allocation.entities.discipline import Discipline
+from allocation.constraints import constraints
+from allocation.constraints.composite import ConstraintComposite
+
+from allocation.sats.brute_force import BruteForceSAT
+from allocation.sats.semantic_tableau import SemanticTableauSAT
+from allocation.sats.dpll import DPLL
+from allocation.sats.pysat_adapter import PySATAdapter
+
+from allocation.utils.cnf.clausal_form_converter import ClausalFormConverter
+from allocation.utils.cnf.cnf_converter import CNFConverter
+from allocation.utils.literal_converter import LiteralConverter
+from allocation.main.execute import Allocator
+
+
+disciplines = get_disciplines()
+schedules = [
+    [], [], [], [], [],
+    [], [], [], [], []
+]
+
+
+def atoms_map(n_disciplines: int, n_schedules: int):
+    dict_map = {}
+    count = 0
+    for i in range(n_disciplines):
+        for j in range(n_schedules):
+            count += 1
+            dict_map[Atom(f'{i + 1}_{j + 1}')] = count
+    return dict_map
+
+
+def to_model(disciplines, n_schedules):
+    composite01 = ConstraintComposite()
+    composite01.add_all([constraint(disciplines, n_schedules) for constraint in constraints])
+    return composite01.apply()
+
+
+model = to_model(disciplines, 3)
+brute_force = BruteForceSAT(model)
+tableau = SemanticTableauSAT({model})
+
+dict_map = atoms_map(len(disciplines), 3)
+cnf_model = ClausalFormConverter.convert(model)
+converter = LiteralConverter(dict_map)
+cnf_model = converter.to_clauses_of_int(cnf_model)
+converter = LiteralConverter(dict_map)
+dpll = DPLL(cnf_model, converter)
+py_sat = PySATAdapter(cnf_model, converter)
+
+
+Allocator(disciplines, 3).execute([brute_force, tableau, dpll, py_sat])
