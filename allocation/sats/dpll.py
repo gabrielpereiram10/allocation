3982d5fa890b75fb909cf3c931ff0fab2e0fbaf2
Gabriel Pereira de Menezes <gabrielpereiram10@gmail.com>
2021-03-27 17:18:37 -0300

    refactor

--- allocation/sats/dpll.py
+++ allocation/sats/dpll.py
@@ -26,7 +26,7 @@ class DPLL(SAT):
             return interpretation
         if set() in clauses:
             return False
-        literal = self._get_literal(clauses)
+        literal = self._choose_literal(clauses)
         left = {frozenset({literal})}.union(clauses.copy())
         result = self._sat(left, interpretation.copy())
         if result:
@@ -34,8 +34,8 @@ class DPLL(SAT):
         right = {frozenset({literal * -1})}.union(clauses.copy())
         return self._sat(right, interpretation.copy())
 
-    def _get_literal(self, clauses: Set[FrozenSet[int]]) -> int:
-        literals = self.get_multi_mode(clauses)
+    def _choose_literal(self, clauses: Set[FrozenSet[int]]) -> int:
+        literals = self._get_multi_mode(clauses)
         ordered_clauses = sorted(clauses, key=len)
         for clause in ordered_clauses:
             intersection_of_literals = set(literals).intersection(clause)
@@ -43,24 +43,33 @@ class DPLL(SAT):
                 return intersection_of_literals.pop()
 
     @staticmethod
-    def get_multi_mode(clauses: Set[FrozenSet[int]]):
+    def _get_multi_mode(clauses: Set[FrozenSet[int]]):
         literals = []
         for clause in clauses:
             literals.extend(list(clause))
         return multimode(literals)
 
-    def _unit_propagation(self, clauses: Set[FrozenSet[int]], interpretation: Set[int]) -> Tuple[ClausesOfIntegers, Set[int]]:
-        unit_clauses_literals = set(map(lambda x: set(x).pop(), filter(self._is_unit_clause, clauses)))
-        if not unit_clauses_literals:
-            return clauses, interpretation
-        new_clauses = set()
-        interpretation = interpretation.union(unit_clauses_literals)
-        for literal in unit_clauses_literals:
-            clauses = set(filter(lambda c: literal not in c, clauses))
-            for clause in clauses:
-                new_clauses.add(clause.difference({literal * -1}))
-        return new_clauses, interpretation
+    def _unit_propagation(self, clauses: Set[FrozenSet[int]], interpretation: Set[int]) -> Tuple[
+        ClausesOfIntegers, Set[int]]:
+        while True:
+            literal = self._literal_unit(clauses)
+            if not literal:
+                break
+            interpretation = interpretation.union({literal})
+            clauses = self._remove_clauses_with_literal(clauses, literal)
+            clauses = self._remove_complement_literal(clauses, literal)
+        return clauses, interpretation
 
     @staticmethod
-    def _is_unit_clause(clause: FrozenSet[int]) -> bool:
-        return len(clause) == 1
+    def _remove_clauses_with_literal(clauses: Set[FrozenSet[int]], literal: int) -> Set[FrozenSet[int]]:
+        return set(filter(lambda c: literal not in c, clauses))
+
+    @staticmethod
+    def _remove_complement_literal(clauses: Set[FrozenSet[int]], literal: int) -> Set[FrozenSet[int]]:
+        return set(map(lambda c: c.difference({literal * -1}), clauses))
+
+    @staticmethod
+    def _literal_unit(clauses: Set[FrozenSet[int]]) -> int:
+        for clause in clauses:
+            if len(clause) == 1:
+                return list(clause)[0]
